6bced0e32069a93338afdc113debd32083d17a6b
Riya Verma <77240974+Riya59@users.noreply.github.com>
2023-05-04 00:20:12 +0530

    Update Buy and sell stock.java

--- Arrays&Strings/Buy and sell stock.java
+++ Arrays&Strings/Buy and sell stock.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int maxProfit(int[] prices) {
+       int n = prices.length;
+       int max = Integer.MAX_VALUE;
+       int day = 0;
+       int buy = 0;
+       for(int i=0;i<n;i++){
+           if(prices[i]<max){
+             max = prices[i];
+           }
+           day = prices[i]-max;
+           if(buy<day){
+               buy = day;
+           }
+       }
+       return buy;
+    }
+}
